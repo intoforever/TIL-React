5f4c2a97aa3266608a180ce4a231f33ea68dcecb
intoforever <startingfrom33@gmail.com>
2025-04-09 20:58:34 +0900

    #my-app
    - main, app 내용 정리

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,13 +1,5 @@
-/**
- * 1. 사용자가 할 일을 입력하고 추가할 수 있다.
- * 2. 추가된 할 일 목록을 화면에 표시한다.
- * 3. 할 일을 클릭하면 완료 상태로 변경된다. (완료를 재클릭시 다시 취소)
- * 4. 완료된 할 일은 취소선으로 표시된다.
- * 5. 할 일을 삭제할 수 있다.
- */
-
-
 import 'styles/App.css'
+
 import TodoList from 'components/TodoList'
 import TodoForm from 'components/TodoForm'
 import TodoFilter from 'components/TodoFilter'
